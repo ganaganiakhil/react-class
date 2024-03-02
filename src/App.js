76f54bbdf3f1b4ebe52d9e1d45aeda686e2458bd
AKHIL GANAGANI <ganaganiakhil@gamil.com>
2024-03-02 12:28:38 +0530

    jsx in react

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Orderedlist from "./components/functional-components/list/order-list/orde
 import Unorderedlist from "./components/functional-components/list/unorder-list/unordered-list"
 import ImageComponent from "./components/functional-components/image/image-component"
 import Tablecomponent from "./components/functional-components/table/table-component"
+import Profile from "./components/functional-components/jsx/jsx"
 
 function App(){
   return(
     <div>
       <h1>hello world</h1>
-      <Buttoncomponent/>
-      <Orderedlist/>
-      <Unorderedlist/>
-      <ImageComponent/>
-      <Tablecomponent/>
+      <Profile/>
     </div>
   )
 }
